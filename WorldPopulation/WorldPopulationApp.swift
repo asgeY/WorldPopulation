3cf066d988091ca1ea7d0f09dbc7a640e2b0b5ac
AsgeY <asmow@yahoo.com>
2023-04-04 20:08:12 -0500

    Initial Commit

---
+++ WorldPopulation/WorldPopulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldPopulationApp.swift
+//  WorldPopulation
+//
+//  Created by AsgeY on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct WorldPopulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
